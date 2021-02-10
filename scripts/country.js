10204e66a8e80a0ed766a17c90b4407248bcc668
DanielDimitrovD <dakata619@gmail.com>
2021-02-10 14:23:49 +0200

    Fix loading bug in country.js

--- scripts/country.js
+++ scripts/country.js
@@ -230,7 +230,7 @@ function generateBorderCountriesNode(countryObject) {
 
 function clearCountryHMTL() {
     document.getElementById("country-name").innerHTML = "";
-    document.getElementById("country-info").innerText = "";
-    document.getElementById("country-borders").innerText = "";
+    document.getElementById("country-container").innerHTML = "";
+    document.getElementById("country-borders").innerHTML = "";
     
 }
